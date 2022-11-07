0a834697b65355139029b541e4fb1e046f05efd4
Yarden-Tal <83368934+Yarden-Tal@users.noreply.github.com>
2022-11-07 22:47:27 +0200

    deploy retry

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,6 @@
-const routerBase =
-  process.env.DEPLOY_ENV === 'GH_PAGES'
-    ? {
-        router: {
-          base: '/nuxt-app/'
-        }
-      }
-    : {}
-
 export default {
-  ...routerBase
-}
+    target: 'static',
+    router: {
+      base: '/nuxt-app/'
+    }
+  }
